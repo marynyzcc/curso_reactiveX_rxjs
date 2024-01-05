cc357a9117f1b84c273e63e2affa0739bdd291d0
Maryny <marynycoll27@gmail.com>
2024-01-05 13:28:59 +0100

    fromEvent

---
+++ src/observables/06-fromEvent.ts
@@ -0,0 +1,22 @@
+import { fromEvent } from "rxjs";
+
+/* Eventos del DOM */
+const src1$ = fromEvent<MouseEvent>(document, 'click');
+const src2$ = fromEvent<KeyboardEvent>(document, 'keyup');
+
+const observer = {
+    next: val => console.log('next', val)
+};
+
+// src1$.subscribe(observer);
+/* src1$.subscribe(ev => {   
+    console.log(ev.x);
+    console.log(ev.y);
+}); */
+
+src1$.subscribe(({x,y}) => {    // desestructuracion, como ya sabemos de qué tipo es el evento 
+    console.log(x, y);          // en este caso MouseEvent, del objeto que se recibe podemos coger las 
+});                             // propiedades que nos interesa, en este caso la propiedad x y la propiedad y.
+src2$.subscribe(evento => {
+    console.log(evento.key);
+});
