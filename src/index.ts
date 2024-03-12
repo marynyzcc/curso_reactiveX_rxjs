d74c5d5b9d1b95e7c97e79007aabbe100f1abe43
Maryny Castada <mzcastada@indra.es>
2024-03-12 16:29:49 +0100

    correcion 07-tap

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { of, take, tap } from "rxjs";
-
-const numeros$ = of(1, 2, 3, 4, 5)
-    .pipe( tap(console.log) );
-
-numeros$.pipe(
-    tap(t => console.log('tap', t)),
-    take(3)
-)
-.subscribe({
-    next: val => console.log('next:', val),
-    complete: () => console.log('complete')
-});
+import {  } from "rxjs";
