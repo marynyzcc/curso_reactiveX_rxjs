1a532cd37272adfb44034e80317cbfee71c98e75
Maryny Castada <mzcastada@indra.es>
2024-03-12 16:28:59 +0100

    take

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-import {  } from "rxjs";
+import { of, take, tap } from "rxjs";
+
+const numeros$ = of(1, 2, 3, 4, 5)
+    .pipe( tap(console.log) );
+
+numeros$.pipe(
+    tap(t => console.log('tap', t)),
+    take(3)
+)
+.subscribe({
+    next: val => console.log('next:', val),
+    complete: () => console.log('complete')
+});
