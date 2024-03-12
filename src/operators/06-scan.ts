644d233feaf01abc90c43855ed92ffcad7a9b7e4
Maryny Castada <mzcastada@indra.es>
2024-03-12 16:14:19 +0100

    scan

---
+++ src/operators/06-scan.ts
@@ -0,0 +1,57 @@
+import { from } from "rxjs";
+import { map, reduce, scan } from "rxjs/operators";
+
+/***************** Operador scan ********************/
+/* El scan es igual que el operador reduce, la unica diferencia 
+* es que el scan emite el valor acumulado en cada iteracion
+*/
+
+const numbers = [1, 2, 3, 4, 5];
+
+// const totalAcumulador = (acc, cur) => {
+//     return acc + cur;
+// };
+
+// const totalAcumulador = (acc, cur) => acc + cur;
+
+// // Reduce
+// from(numbers).pipe(
+//     reduce(totalAcumulador, 0)
+// )
+// .subscribe(console.log);
+
+// // Scan
+// from(numbers).pipe(
+//     scan(totalAcumulador, 0)
+// )
+// .subscribe(console.log);
+
+/* Redux:
+* es un patron que permite manejar el estado global de la aplicacion 
+* en un solo objeto
+*/
+interface Usuario {
+    id?: string;
+    autenticado?: boolean;
+    token?: string;
+    edad?: number;
+}
+
+const user: Usuario[] = [
+    { id: 'fher', autenticado: false, token: null },    
+    { id: 'fher', autenticado: true, token: 'ABC' },    
+    { id: 'fher', autenticado: true, token: 'ABC123' }
+];
+
+const state$ = from( user ).pipe(
+    scan<Usuario, Usuario>( (acc, cur) => {
+         return { ...acc, ...user.reduce((a, b) => ({ ...a, ...b }), {}) }
+     }, { edad: 33 })
+);
+
+
+const id$ = state$.pipe(
+    map(state => state.id)
+);
+
+id$.subscribe(console.log);
