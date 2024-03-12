c950eac16441dc9c092e746f267cb07271a49e10
Maryny Castada <mzcastada@indra.es>
2024-03-12 13:26:05 +0100

    filter

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,38 @@
-import { fromEvent, range } from "rxjs";
-import { map, mapTo, pluck } from "rxjs/operators";
+import { from, range } from "rxjs";
+import { filter } from "rxjs/operators";
 
-/********* Operador map **********/
-/* Mostrar los numeros del 1 al 5 */
-// range(1,5).subscribe( console.log );
-
-
-/* Mostrar los numeros de 10 en 10 */
-// range(1,5).pipe(
-//     map<number, number>( val => val * 10)
-//     map<number, string>( val => (val * 10).toString())
+/***************** Operador filter ********************/
+// range(1,10).pipe(
+//     filter( (val, i) => { 
+//         console.log("index: ", i)
+//         return val % 2 === 1
+//     })
 // )
 // .subscribe( console.log );
 
-/* Mostrar el codigo de la tecla pulsada */
-// const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup' );
+interface Personaje {
+    tipo: string;
+    nombre: string;
+}
 
-// const keyupCode$ = keyup$.pipe(
-//     map(event => event.code)
-// );
+const personajes: Personaje[] = [
+    {
+        tipo: "heroe",
+        nombre: "Batman"
+    },
+    {
+        tipo: "heroe",
+        nombre: "Robin"
+    },
+    {
+        tipo: "villano",
+        nombre: "Joker"
+    }
+];
 
-// keyupCode$.subscribe( code => console.log('map', code));
+from( personajes ).pipe(
+    filter( p => {    
+        return p.tipo === "heroe"
+    })
 
-
-/********* Operador pluck **********/
-// const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup' );
-
-// const keupPluck$ = keyup$.pipe(
-//     // pluck('key')
-//     pluck('target', 'baseURI') // la coma equivale a la notacion de punto
-// );
-
-// keupPluck$.subscribe( code => console.log('pluck', code));
-
-
-/********* Operador mapTo **********/
-const keyup$ = fromEvent<KeyboardEvent>( document, 'keyup' );
-
-const keyupMapTo$ = keyup$.pipe(
-    mapTo('tecla presionada')
-);
-
-keyupMapTo$.subscribe( code => console.log('mapTo'));
+).subscribe( console.log )
