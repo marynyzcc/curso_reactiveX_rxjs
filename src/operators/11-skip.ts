71b680305cb07d953d638e1908e8c3e18b12c99d
Maryny Castada <mzcastada@indra.es>
2024-03-14 11:24:41 +0100

    takeUntil y skip

---
+++ src/operators/11-skip.ts
@@ -0,0 +1,29 @@
+import { fromEvent, interval } from "rxjs";
+import { takeUntil, skip, tap } from "rxjs/operators";
+
+
+/***************** Operador skip ********************/
+/* Se quiere detener el counter$ si se pulsa el boton 2 veces */
+
+const boton = document.createElement('button');
+boton.innerHTML = 'Detener Timer';
+
+document.querySelector('body').append( boton );
+
+const counter$ = interval(1000);
+// const clickBtn$ = fromEvent( boton, 'click' );
+const clickBtn$ = fromEvent( boton, 'click' ).pipe(
+    tap(() => console.log('tap antes de skip')),
+    skip(1),
+    tap(() => console.log('tap despues de skip'))
+)
+
+counter$.pipe(
+    takeUntil( clickBtn$ )
+)
+.subscribe({
+    next: val => console.log('next', val),  
+    complete: () => console.log('complete')
+});
+
+
